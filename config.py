cdd804bad7385ab2bb428c0c79c2a54d727f94e4
Lord Mike <lordmike@iki.fi>
2020-03-21 13:02:20 +0200

    there is no need for source info :)

--- config.py
+++ config.py
@@ -134,6 +134,7 @@ def append_plugins(ecli_elem: Element):
     name.text = 'pydev'
     version = ET.SubElement(plugin_pydev, Tag.version)
     version.text = '7.4.0'
+    '''
     plugin_pydev.append(ET.Comment(' {version} is replaced with value from tag "version" '))
     installer_file = ET.SubElement(plugin_pydev, Tag.installer_file)
     #installer_file.text = 'PyDev%20{version}.zip'
@@ -141,7 +142,8 @@ def append_plugins(ecli_elem: Element):
     plugin_pydev.append(ET.Comment(' {installer_file} is replaced with value from tag "installer_file" '))
     installer_url = ET.SubElement(plugin_pydev, Tag.installer_url)
     installer_url.text = 'https://sourceforge.net/projects/pydev/files/pydev/PyDev%20{version}/{installer_file}/download'
-    plugin_pydev.append(ET.Comment(' {version} is replaced with value from tag "version" '))
+    '''
+    #plugin_pydev.append(ET.Comment(' {version} is replaced with value from tag "version" '))
     #install_path = ET.SubElement(plugin_pydev, Tag.install_path)
     # TODO: get this path from eclipse
     #install_path.text = 'C:\\Program Files\\eclipse-{version}'
