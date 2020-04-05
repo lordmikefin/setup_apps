46da6c907fd9b77512ce588917bdd988f5522dcb
Lord Mike <lordmike@iki.fi>
2020-04-05 16:08:53 +0300

    version "0.1.10"

--- __init__.py
+++ __init__.py
@@ -20,8 +20,8 @@
 """
 
 __license__ = "MIT License"
-__version__ = "0.1.9"
-__revision__ = "setup_apps (module)  v" + __version__ + " (2020-03-16)"
+__version__ = "0.1.10"
+__revision__ = "setup_apps (module)  v" + __version__ + " (2020-04-05)"
 
 
 '''
