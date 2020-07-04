18a07311c6ac7c3cbd4ba5bf4adc17aaea160e21
Lord Mike <lordmike@iki.fi>
2020-07-04 13:50:42 +0300

    do not continue after it is not possible :)

--- npp.py
+++ npp.py
@@ -84,6 +84,7 @@ class Npp(Base):
     def download(self) -> bool:
         if not (self.url_ok and self.path_ok):
             logger.error('Can not download Notepad++ installer.')
+            return False
 
         if not self.checksum:
             logger.error('Checksum data missing.')
