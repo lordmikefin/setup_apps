79bfb1ffc40f1d46a99bec3e778c475204b08b7c
Lord Mike <lordmike@iki.fi>
2020-07-29 18:20:44 +0300

    version 0.1.25

--- __init__.py
+++ __init__.py
@@ -20,8 +20,8 @@
 """
 
 __license__ = "MIT License"
-__version__ = "0.1.24"
-__revision__ = "setup_apps (module)  v" + __version__ + " (2020-07-23)"
+__version__ = "0.1.25"
+__revision__ = "setup_apps (module)  v" + __version__ + " (2020-07-29)"
 
 import sys
 
