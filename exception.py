754fc7dc1c846cc8b6fff75ff0980715afca7a62
Lord Mike <lordmike@iki.fi>
2021-03-21 17:00:10 +0200

    Special case exceptions :)

---
+++ exception.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+"""
+    exception.py
+    ~~~~~~~~~~~~
+
+    Special case exceptions :)
+
+    License of this script file:
+       MIT License
+
+    License is available online:
+      https://github.com/lordmikefin/setup_apps/blob/master/LICENSE
+
+    Latest version of this script file:
+      https://github.com/lordmikefin/setup_apps/blob/master/exception.py
+
+
+    :copyright: (c) 2021, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+    :license: MIT License
+"""
+
+class NotSudo(Exception):
+    pass
